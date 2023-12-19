ba3419e0292acbc0d1a94410e678b5f90e34bfbd
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-20 00:31:56 +0530

    no dahej init

---
+++ src/app/_components/Form.tsx
@@ -0,0 +1,90 @@
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export default function Form() {
+  return (
+    <form>
+      <div className="space-y-12 p-5">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Calculate your Dahej
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            We&apos;ll always let you know about important changes, but you pick
+            what else you want to hear about.
+          </p>
+
+          <div className="mt-10 space-y-10">
+            <fieldset>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Proffession
+              </legend>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                Well sought after proffessions get more from the groom&apos;s
+              </p>
+              <div className="mt-6 space-y-6">
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-everything"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-orange-600 focus:ring-orange-600"
+                  />
+                  <label
+                    htmlFor="push-everything"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Engineer
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-email"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-orange-600 focus:ring-orange-600"
+                  />
+                  <label
+                    htmlFor="push-email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Doctor
+                  </label>
+                </div>
+                <div className="flex items-center gap-x-3">
+                  <input
+                    id="push-nothing"
+                    name="push-notifications"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-orange-600 focus:ring-orange-600"
+                  />
+                  <label
+                    htmlFor="push-nothing"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Buisnessman
+                  </label>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6 p-5">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+        >
+          <Link href="/result">Calculate</Link>
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div className="px-6 py-12 sm:py-16 lg:px-4">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-orange-600">
+          PAISA HI PAISA
+        </p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Dahej ka Hisab
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          अपने दहेज का हिसाब करे
+        </p>
+      </div>
+    </div>
+  );
+}
